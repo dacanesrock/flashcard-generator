6a144b57ed9e3162fb9923e19fa2ba49df44e6b3
Robert Geyer <Robert Geyer>
2017-05-06 10:56:19 -0400

    added comment

--- cli.js
+++ cli.js
@@ -1,4 +1,6 @@
 var MainApp = require("./MainApp.js");
 var prompts = require("./prompts");
 
+// this originally had a lot more stuff in it. then i moved it all around and haven't organized this to be more robust.
+
 prompts();
